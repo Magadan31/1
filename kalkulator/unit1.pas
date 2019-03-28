be4d7cb0f624482fe0deca19a8dd65844aaff161
Magadan31 <49016857+Magadan31@users.noreply.github.com>
2019-03-28 09:39:47 +0300

    Add files via upload

---
+++ kalkulator/unit1.pas
@@ -0,0 +1,111 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Button6: TButton;
+    Button7: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
+    procedure Button6Click(Sender: TObject);
+    procedure Button7Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+
+procedure TForm1.Button1Click(Sender: TObject);
+var a,b,result:real;
+begin
+a:=StrToFloat(edit1.Text);
+b:=StrToFloat(edit2.Text);
+result:=a+b;
+edit3.Text:=FloatToStr(result);
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var a,b,result:real;
+begin
+a:=StrToFloat(edit1.Text);
+b:=StrToFloat(edit2.Text);
+result:=a*b;
+edit3.Text:=FloatToStr(result);
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+var a,b,result:real;
+begin
+a:=StrToFloat(edit1.Text);
+b:=StrToFloat(edit2.Text);
+result:=a-b;
+edit3.Text:=FloatToStr(result);
+end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+var a,b,result:real;
+begin
+a:=StrToFloat(edit1.Text);
+b:=StrToFloat(edit2.Text);
+result:=a/b;
+edit3.Text:=FloatToStr(result);
+end;
+
+procedure TForm1.Button5Click(Sender: TObject);
+var a,result:real;
+begin
+a:=StrToFloat(edit1.Text);
+result:=sqr(a);
+edit3.Text:=FloatToStr(result);
+end;
+
+procedure TForm1.Button6Click(Sender: TObject);
+var a,result:real;
+begin
+a:=StrToFloat(edit1.Text);
+result:=sqrt(a);
+edit3.Text:=FloatToStr(result);
+end;
+
+procedure TForm1.Button7Click(Sender: TObject);
+var a,b,result:real;
+begin
+a:=StrToFloat(edit1.Text);
+result:=a/100*b;
+edit3.Text:=FloatToStr(result);
+end;
+
+end.
